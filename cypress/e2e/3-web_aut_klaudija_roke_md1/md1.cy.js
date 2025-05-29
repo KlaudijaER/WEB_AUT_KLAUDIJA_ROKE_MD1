73f9b5148f5c7b7af7a2912a018b9e831c577ab0
KlaudijaER <klaudijaemilija.roke@gmail.com>
2025-05-29 14:34:51 +0300

    adding the correct code for md1

--- cypress/e2e/3-web_aut_klaudija_roke_md1/md1.cy.js
+++ cypress/e2e/3-web_aut_klaudija_roke_md1/md1.cy.js
@@ -7,19 +7,22 @@ describe("Selectable Grid Test", () => {
   });
 
   it("highlighting even numbers", () => {
-    const toSelect = ["Two", "Four", "Six", "Eight"];
-    const notSelected = ["One", "Three", "Five", "Seven", "Nine"];
+    SelectablePage.clickItem("Two");
+    SelectablePage.clickItem("Four");
+    SelectablePage.clickItem("Six");
+    SelectablePage.clickItem("Eight");
 
-    toSelect.forEach(item => {
-      SelectablePage.clickItem(item);
-    });
+    // Check that the selected ones are active
+    SelectablePage.shouldBeActive("Two");
+    SelectablePage.shouldBeActive("Four");
+    SelectablePage.shouldBeActive("Six");
+    SelectablePage.shouldBeActive("Eight");
 
-    toSelect.forEach(item => {
-      SelectablePage.shouldBeActive(item);
-    });
-
-    notSelected.forEach(item => {
-      SelectablePage.shouldNotBeActive(item);
-    });
+    // Check that the others are not active
+    SelectablePage.shouldNotBeActive("One");
+    SelectablePage.shouldNotBeActive("Three");
+    SelectablePage.shouldNotBeActive("Five");
+    SelectablePage.shouldNotBeActive("Seven");
+    SelectablePage.shouldNotBeActive("Nine");
   });
 });
